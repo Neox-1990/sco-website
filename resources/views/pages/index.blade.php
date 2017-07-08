78170bc01319caec517bbebdc4ef3d5e41e85811
Ronald Großmann <dev@ronaldg.net>
2017-07-09 01:16:05 +0200

    Lorem Ipsum placeholderside

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Testpage</title>
+    <link href="https://fonts.googleapis.com/css?family=Kavivanar" rel="stylesheet">
+    <style media="screen">
+      *{
+        margin:0;
+        padding:0;
+      }
+      body{
+        display:flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        width: 100vw;
+        height: 100vh;
+        font-family: 'Kavivanar', cursive;
+        font-size: 10rem;
+      }
+    </style>
+  </head>
+  <body>
+    <div class="">
+      Lorem Ipsum
+    </div>
+  </body>
+</html>
