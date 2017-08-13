565264aaa131186f781072eb54dd18ea9f80f030
Ronald Großmann <dev@ronaldg.net>
2017-08-13 03:19:43 +0200

    Bugfix indexview Teams

--- resources/views/teams/index.blade.php
+++ resources/views/teams/index.blade.php
@@ -29,20 +29,20 @@
           <tbody>
             @foreach ($class['confirmed'] as $team)
               <tr>
-                <td>{{$team->number}}</td>
-                <td><a href="/teams/{{$team['id']}}">{{$team->name}}</a></td>
-                <td>{{config('constants.car_names')[$team->car]}}</td>
-                <td>{{$team->user()->name}}</td>
-                <td class="text-success">{{config('constants.status_names')[$team->status]}}</td>
+                <td>{{$team['number']}}</td>
+                <td><a href="/teams/{{$team['id']}}">{{$team['name']}}</a></td>
+                <td>{{config('constants.car_names')[$team['car']]}}</td>
+                <td>{{$team['user']['name']}}</td>
+                <td class="text-success">{{config('constants.status_names')[$team['status']]}}</td>
               </tr>
             @endforeach
             @foreach ($class['waiting'] as $team)
               <tr>
-                <td>{{$team->number}}</td>
-                <td><a href="/teams/{{$team['id']}}">{{$team->name}}</a></td>
-                <td>{{config('constants.car_names')[$team->car]}}</td>
-                <td>{{$team->user()->name}}</td>
-                <td class="text-warning">{{config('constants.status_names')[$team->status]}}</td>
+                <td>{{$team['number']}}</td>
+                <td><a href="/teams/{{$team['id']}}">{{$team['name']}}</a></td>
+                <td>{{config('constants.car_names')[$team['car']]}}</td>
+                <td>{{$team['user']['name']}}</td>
+                <td class="text-warning">{{config('constants.status_names')[$team['status']]}}</td>
               </tr>
             @endforeach
             @foreach ($class['pending'] as $team)
