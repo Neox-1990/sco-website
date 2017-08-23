3cb9b6e9b9f7b8e0db2b20432183bda82bf1f5df
Ronald Großmann <dev@ronaldg.net>
2017-08-23 23:31:11 +0200

    Footer update -> brand colors for social media links

--- resources/views/master/footer.blade.php
+++ resources/views/master/footer.blade.php
@@ -5,9 +5,9 @@
     </div>
     <div class="m-2">
       <small>Visit us on:</small>
-      <a href="https://www.facebook.com/coresimracing" class="ml-2"><i class="fa fa-facebook-official fa-lg" aria-hidden="true"></i></a>
-      <a href="https://twitter.com/coresimracing" class="ml-2"><i class="fa fa-twitter-square fa-lg" aria-hidden="true"></i></a>
-      <a href="https://www.youtube.com/user/FireMike15NBA" class="ml-2"><i class="fa fa-youtube-play fa-lg" aria-hidden="true"></i></a>
+      <a href="https://www.facebook.com/coresimracing" class="ml-2 fblink"><i class="fa fa-facebook-official fa-lg" aria-hidden="true"></i></a>
+      <a href="https://twitter.com/coresimracing" class="ml-2 twlink"><i class="fa fa-twitter-square fa-lg" aria-hidden="true"></i></a>
+      <a href="https://www.youtube.com/user/FireMike15NBA" class="ml-2 ytlink"><i class="fa fa-youtube-play fa-lg" aria-hidden="true"></i></a>
     </div>
     <div class="m-2">
       <small>SCO Website &copy; 2017 by <a href="mailto:dev@ronaldg.de">Ronald Großmann</a></small>
