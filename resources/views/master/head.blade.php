1335ae1560c58585dcde6861d5702ff3f8fa11ea
Ronald Großmann <dev@ronaldg.net>
2017-08-24 21:06:49 +0200

    favicons added

--- resources/views/master/head.blade.php
+++ resources/views/master/head.blade.php
@@ -2,6 +2,12 @@
 <meta name="description" content="Signupsite for Sports Car Open, an iRacing endurance raceseries by CoRe Simracing">
 <meta name="keywords" content="CoRe, Simracing, iRacing, Sports Car Open, endurance, race">
 <meta name="author" content="Ronald Großmann">
+<link rel="shortcut icon" href="/favicon.ico">
+<link rel="icon" type="image/png" href="/favicon_ico.png" sizes="32x32">
+<link rel="apple-touch-icon" sizes="180x180" href="/favicon_apple.png">
+<meta name="msapplication-TileColor" content="#ffffff">
+<meta name="msapplication-TileImage" content="/favicon_ms.png">
+<link rel="apple-touch-icon-precomposed" sizes="180x180" href="/favicon_apple.png">
 
 <link rel="stylesheet" href="{{asset('css/app.css')}}">
 <link rel="stylesheet" href="{{asset('css/font-awesome.min.css')}}">
