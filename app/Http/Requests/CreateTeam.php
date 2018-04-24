b072b08ed488125c41904cedb4b4d7fde0d987cc
Ronald Großmann <dev@ronaldg.net>
2018-04-24 21:18:11 +0200

    nummerfix

--- app/Http/Requests/CreateTeam.php
+++ app/Http/Requests/CreateTeam.php
@@ -29,7 +29,7 @@ class CreateTeam extends FormRequest
         return [
           'teamname' => 'required|max:255',
           'teamcar' => 'required|integer',
-          'teamnumber' => 'required|integer|max:199',
+          'teamnumber' => 'required|integer|max:999',
           'iracing_teamid' => 'required|integer|max:9999999',
 
           'driver1.name' => 'required|max:255',
