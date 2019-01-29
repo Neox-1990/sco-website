ff3b1938fb42693ac75799b7042c23828512e647
Ronald Großmann <dev@ronaldg.de>
2019-01-29 23:00:47 +0100

    updated css and js timestampfile

--- resources/views/master/head.blade.php
+++ resources/views/master/head.blade.php
@@ -11,5 +11,5 @@
 <meta name="csrf-token" content="{{ csrf_token() }}">
 <title>Sports Car Open</title>
 
-<link rel="stylesheet" href="{{asset('css/app.css')}}?date=20180714">
+<link rel="stylesheet" href="{{asset('css/app.css')}}?date=20190129">
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" integrity="sha384-gfdkjb5BdAXd+lj+gudLWI+BXq4IuLW5IT+brZEZsLFm++aCMlF1V92rMkPaX4PP" crossorigin="anonymous">
