0e711000c4b9dc96ea74ffa6b8313b46337c5421
Ronald Großmann <dev@ronaldg.de>
2019-01-31 17:10:53 +0100

    Facebook quickfix

--- app/Social/FacebookHelper.php
+++ app/Social/FacebookHelper.php
@@ -2,6 +2,8 @@
 
 namespace App\Social;
 
+use Exception;
+
 use \Facebook\Facebook;
 
 /**
@@ -19,8 +21,8 @@ class FacebookHelper
     {
         $this->app_id = config('services.facebook')['app_id'];
         $this->app_secret = config('services.facebook')['app_secret'];
-        $this->default_graph_version = 'v2.10';
-        $this->default_access_token = config('services.facebook')['app_id'].'|'.config('services.facebook')['app_secret'];
+        $this->default_graph_version = 'v3.2';
+        $this->default_access_token = config('services.facebook')['app_access_token'];
     }
 
     public function getTextFeedElements($limit = 10)
@@ -32,11 +34,16 @@ class FacebookHelper
           'default_access_token' => $this->default_access_token, // optional
         ]);
         $pageid = \App\Setting::getSetup()['facebookpageid'];
-        $response = $fb->get('/'.$pageid.'/posts?fields=id,message,story,created_time,full_picture,link');
-        $responseData = ($response->getDecodedBody())['data'];
-        $filteredData = array_filter($responseData, function ($data) {
+        try{
+          $response = $fb->get('/'.$pageid.'/posts?fields=id,message,story,created_time,full_picture,link');
+          $responseData = ($response->getDecodedBody())['data'];
+          $filteredData = array_filter($responseData, function ($data) {
             return !array_key_exists('story', $data) && array_key_exists('message', $data);
-        });
+          });
+        }catch(Exception $e){
+          //dd($e);
+          $filteredData = [];
+        }
         return array_slice($filteredData, 0, min($limit, sizeof($filteredData)));
     }
 }
