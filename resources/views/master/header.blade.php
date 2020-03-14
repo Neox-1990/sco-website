85211173db0c87b40273ed57c88f9ad7a702b24b
Ronald Großmann <dev@ronaldg.de>
2020-03-14 01:57:38 +0100

    Header Image and Logo for first 1of 2020

--- resources/views/master/header.blade.php
+++ resources/views/master/header.blade.php
@@ -1,5 +1,5 @@
 <header class="sco-header">
   <div class="container logo pl-5 pr-2 px-md-0 py-0 pt-5 pt-md-0">
-    <img class="mx-auto d-block" src="/img/SCO2K19_LOGO_Alternate_coloured.png" alt="">
+    <img class="mx-auto d-block" src="/img/SCOI1000_WebsiteBanner.png" alt="">
   </div>
 </header>
