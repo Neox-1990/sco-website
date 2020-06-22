0823613928c7291ee1545bf7cf7c03372ec7f323
Ronald Großmann <dev@ronaldg.de>
2020-06-22 17:21:39 +0200

    Updated Mails for VIR300

--- app/Mail/StatusUpdateMail.php
+++ app/Mail/StatusUpdateMail.php
@@ -51,10 +51,10 @@ class StatusUpdateMail extends Mailable
           $info['text'] = 'As soon as a confirmed team withdraws or gets withdrawn from the event, the next team on the waiting list (for that class) gets confirmed. The order of the waiting list depends on the sign-up date of the team.';
           break;
         case 3:
-          $info['html'] = '<p>Your team is qualified for the event. To get confirmed you now have to pay the season entry fee of $9 until Sunday, May 24th, 2020 at 23:59 UTC. You can use the following paypal.me link to pay the fee:<br><br><a href="https://paypal.me/sportscaropen/9usd">https://paypal.me/sportscaropen/9usd</a><br><br><b>Make sure to send a note with your payment, so we know what team/car/entry that payment is meant for!</b></p>';
-          $info['text'] = 'Your team is qualified for the event. To get confirmed you now have to pay the season entry fee of $9 until Sunday, May 24th, 2020 at 23:59 UTC. You can use the following paypal.me link to pay the fee:
+          $info['html'] = '<p>Your team is qualified for the event. To get confirmed you now have to pay the season entry fee of $6 until Sunday, July 12th, 2020 at 23:59 UTC. You can use the following paypal.me link to pay the fee:<br><br><a href="https://paypal.me/sportscaropen/6usd">https://paypal.me/sportscaropen/6usd</a><br><br><b>Make sure to send a note with your payment, so we know what team/car/entry that payment is meant for!</b></p>';
+          $info['text'] = 'Your team is qualified for the event. To get confirmed you now have to pay the season entry fee of $6 until Sunday, July 12th, 2020 at 23:59 UTC. You can use the following paypal.me link to pay the fee:
 
-https://paypal.me/sportscaropen/9usd
+https://paypal.me/sportscaropen/6usd
 
 # Make sure to send a note with your payment, so we know what team/car/entry that payment is meant for! #';
           break;
