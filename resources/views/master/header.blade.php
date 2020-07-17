230f8d23a34a672311f8f92b3be4fd7dbc0213b8
Ronald Großmann <dev@ronaldg.de>
2020-07-17 20:12:33 +0200

    Update Header Images for Season 10

--- resources/views/master/header.blade.php
+++ resources/views/master/header.blade.php
@@ -1,5 +1,5 @@
 <header class="sco-header">
   <div class="container logo pl-5 pr-2 px-md-0 py-0 pt-5 pt-md-0">
-    <img class="mx-auto d-block" src="/img/sco2020-oaktree300-bannerlogo.png" alt="">
+    <img class="mx-auto d-block" src="/img/SCO2K20_LOGO_Alternate.png" alt="">
   </div>
 </header>
